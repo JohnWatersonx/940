8c054186f195bb093541b11cd87f8b8ed710be23
JohnWatersonx <urh21903@gmail.com>
2025-05-04 20:42:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.post("/depot", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
